f7b23eaa436c841e249993357dc7fb5b299480d7
Kenton Hertzog <Kenton@Hertzogs.com>
2026-07-21 18:03:46 -0400

    Exercise !

---
+++ src/app/01-vapor/loading.js
@@ -0,0 +1,19 @@
+import LibraryGameCardSkeleton from "@/components/LibraryGameCardSkeleton";
+import { range } from "@/utils";
+
+export default function Loading() {
+    return (
+    <section className="max-width-wrapper">
+      <header className="library-header">
+        <h1>My games</h1>
+      </header>
+      <div className="game-grid">
+        {range(0,11).map((index) => (
+          <LibraryGameCardSkeleton
+            key={index}
+          />
+        ))}
+      </div>
+    </section>
+    );
+}
